db2e312fcca62f1bacdc266d0c3aa04977d7d360
edgarhdezrguez <37212615+edgarhdezrguez@users.noreply.github.com>
2019-09-01 23:28:41 +0100

    upload .java files

---
+++ practica_cadebay/main/ConsoleComand.java
@@ -0,0 +1,54 @@
+package main;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ConsoleComand {
+
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+
+        while (!salir) {
+
+            System.out.println("1. Listar usuarios activos");
+            System.out.println("2. Listar las ciudades cuya ciudad empiece por lo que inserte.");
+            System.out.println("3. Listar los usuarios por fecha de creación (Ascendente y Descendente)");
+            System.out.println("4. Añadir un usuario");
+            System.out.println("5. Salir");
+
+            try {
+
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                    	
+                        System.out.println("Has seleccionado la opcion 1");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion 3");
+                        break;
+                    case 4:
+                        System.out.println("Has seleccionado la opcion 4");
+                        break;
+                    case 5:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 5");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+
+    }
+
+}
